e1857628e2e4875dfb1535dd8e697e34679c2db7
tiwaribaba <niraj.cbmpl@gmail.com>
2016-01-05 16:24:09 +0530

    Second Commit

--- app/src/main/java/com/example/hppc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hppc/myapplication/MainActivity.java
@@ -23,6 +23,9 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
